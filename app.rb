a853e0b74e8a67321a0e47b40aa95600e3e2931f
Kapildell2 <kapilnakhwa@gmail.com>
2015-02-14 17:45:38 +0545

    Added the edit action

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ Bundler.require
 
 require './idea'
 class IdeaBoxApp < Sinatra::Base
+  
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -18,10 +21,29 @@ class IdeaBoxApp < Sinatra::Base
   post '/' do
     # params.inspect
     idea = Idea.new(params['idea_title'],params['idea_description'])
-
     idea.save
+  end
 
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+      redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data ={
+      :title=> params['idea_title'],
+      :description=>params['idea_description']
+    }
+    
+    Idea.update(id.to_i, data)
     redirect '/'
   end
+
+
 
 end
